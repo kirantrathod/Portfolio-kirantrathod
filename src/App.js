e7171df8bfc01f709c04016e227eb6922e798ced
kirantrathod <kirantrathod.kr@gmail.com>
2021-06-28 22:10:57 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ProfileUndraw from "./assets/undraw_Profile.svg";
 
 function App() {
   return (
     <div className="App">
-      <p>Dishant here</p>
+      <header>
+        <nav className="navigation">
+          <div className="nav-container">
+            <p>Home</p>
+            <p>Projects</p>
+            <p>Blogs</p>
+          </div>
+        </nav>
+        <section className="intro-card">
+          <div className="intro-container">
+            <img
+              className="undraw-profile"
+              src={ProfileUndraw}
+              alt="profile_undraw"
+            ></img>
+            <p className="intro-header">
+              <h3>
+                <strong>Hey! I am Kiran Rathod.</strong>
+              </h3>
+            </p>
+          </div>
+        </section>
+      </header>
     </div>
   );
 }
